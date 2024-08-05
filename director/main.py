03f1bc0d85d6d42017154ba9ee52c222a73d0fe0
Colin Jemmott <cjemmott@gmail.com>
2024-08-04 22:51:52 -0700

    Add mqtt output to main

--- director/main.py
+++ director/main.py
@@ -6,21 +6,27 @@ TO DO
 #import board
 #import adafruit_vl53l1x
 import pyaudio
+import board
 import numpy as np
 import audio
+import adafruit_vl53l1x
 import params
+import paho.mqtt.client as mqtt
+
 
 # testing only
-import testing_distance_mock
-vl53 = testing_distance_mock.get_distance()
+#import testing_distance_mock
+#vl53 = testing_distance_mock.get_distance()
 
 def main():
 
     # distance sensor setup
-    #i2c = board.I2C()  # uses board.SCL and board.SDA
-    #vl53 = adafruit_vl53l1x.VL53L1X(i2c)
-    #vl53.start_ranging()
-
+    i2c = board.I2C()  # uses board.SCL and board.SDA
+    vl53 = adafruit_vl53l1x.VL53L1X(i2c)
+    vl53.start_ranging()
+    
+    client = mqtt.Client()
+    client.connect("localhost", 1883, 60)
 
     # Initialize PyAudio
     p = pyaudio.PyAudio()
@@ -37,6 +43,10 @@ def main():
             distance = vl53.distance
 
             mode, freq, amplitude, smoothed_dist, waveform = theremin.update(mode, distance)
+
+            client.publish("moot/distance", smoothed_dist)
+            client.publish("moot/speed", freq/params.MAX_FREQ)
+            client.publish("moot/freq", freq)
 
             pa_stream.write(waveform.astype(np.float32).tobytes())
     finally:
