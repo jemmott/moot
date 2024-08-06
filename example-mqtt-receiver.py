c847dd67ee67c9ebec321721bb513e26fe8bca3a
Marcus Wanner <marcus@wanners.net>
2024-08-06 07:42:08 -0700

    mqtt examples and systemd unit files + ansible WIP

---
+++ example-mqtt-receiver.py
@@ -0,0 +1,21 @@
+import paho.mqtt.client as mqtt
+
+# Define the callback function to handle incoming MQTT messages
+def on_message(client, userdata, msg):
+    if msg.topic == "moot/mode":
+        mode = msg.payload.decode()
+
+# MQTT setup
+mqtt_broker = "localhost"
+mqtt_topic = ["moot/mode"]
+
+client = mqtt.Client()
+client.on_message = on_message
+
+client.connect(mqtt_broker, 1883, 60)
+for t in mqtt_topic: client.subscribe(t)
+
+# Start the MQTT client loop
+print("loading complete, looping forever")
+client.loop_forever()
+
