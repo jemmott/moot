46de181afb64afad67fb990d63dc8f4f920aec9d
Marcus Wanner <marcus@wanners.net>
2024-08-11 19:43:12 -0700

    prep pixelblaze driver

--- lights/pb-driver.py
+++ lights/pb-driver.py
@@ -1,6 +1,6 @@
 import paho.mqtt.client as mqtt
 from pixelblaze import Pixelblaze
-import json
+import json, threading
 
 # Pixelblaze setup
 pixelblaze_ip = "192.168.0.108"
@@ -8,27 +8,38 @@ print(f"waiting on pixelblaze at {pixelblaze_ip}")
 pb = Pixelblaze(pixelblaze_ip)
 print("pb connected")
 
-# pb.setActivePatternByName("standby")
+#pb.setActivePatternByName("standby")
+
+l = threading.Lock()
+global cur_mode
+cur_mode = None
 
 # Define the callback function to handle incoming MQTT messages
 def on_message(client, userdata, msg):
-    if msg.topic == "moot/speed":
-        speed = float(msg.payload.decode())
-        print(f"speed update: {speed}")
-        pb.setActiveVariables({"speed": speed})
-    if msg.topic == "moot/brightness":
-        b = float(msg.payload.decode())
-        pb.setBrightnessSlider(b)
-        print(f"brightness update: {b}")
-    if msg.topic == "moot/mode":
-        mode = msg.payload.decode()
-        print(f"mode update: {mode}")
-        pb.setActiveVariables({"moot_mode": mode})
-        pb.setActivePatternByName(mode)
-
+    global cur_mode, l
+    if not l.acquire(blocking=False):
+        return
+    try:
+        if msg.topic == "moot/speed":
+            speed = float(msg.payload.decode())
+            print(f"speed update: {speed}")
+            pb.setActiveVariables({"speed": speed})
+        if msg.topic == "moot/brightness":
+            b = float(msg.payload.decode())
+            pb.setBrightnessSlider(b)
+            print(f"brightness update: {b}")
+        if msg.topic == "moot/mode":
+            mode = msg.payload.decode()
+            if mode == cur_mode: return
+            print(f"mode update: {mode}")
+            #if mode == "active": mode = "activealt"
+            pb.setActivePatternByName(mode)
+            cur_mode = mode
+    finally:
+        l.release()
 
 # MQTT setup
-mqtt_broker = "localhost"
+mqtt_broker = "192.168.0.103"
 mqtt_topic = ["moot/speed", "moot/brightness", "moot/mode"]
 
 client = mqtt.Client()
@@ -40,3 +51,4 @@ client.connect(mqtt_broker, 1883, 60)
 # Start the MQTT client loop
 print("connected to MQTT and PixelBlaze, looping forever")
 client.loop_forever()
+
