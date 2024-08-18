59cf31f0d65d8a803fe74014081e4ca77083995c
Marcus Wanner <marcus@wanners.net>
2024-08-18 10:59:18 -0700

    unclutter

--- video_server/main.py
+++ video_server/main.py
@@ -11,6 +11,9 @@ cap = cv2.VideoCapture("MOOT_old.mov")
 cap_boot = cv2.VideoCapture("Startup_1.mov")
 cap_shutdown = cv2.VideoCapture("Shutdown_1.mov")
 
+#housekeeping
+os.system("unclutter -idle 1.0")
+
 if use_playback_delay:
     delay = 1  # play as fast as possible
 else:
